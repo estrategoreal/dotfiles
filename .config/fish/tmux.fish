3e36a0e3b5be776dd29ba38ceeef9a9e04e9a2c8
Yoshiji Kochi <yoshiji.kochi@gmail.com>
2021-06-29 19:22:06 +0900

    updated tmux.fish.

--- .config/fish/tmux.fish
+++ .config/fish/tmux.fish
@@ -24,7 +24,7 @@ end
 
 function tmux_automatically_attach_session
   if is_screen_or_tmux_running
-    not is_exists 'tmux'; and return 1
+    not is_exists 'tmux' && return 1
 
     if is_tmux_runnning
       set_color --bold red
@@ -38,24 +38,24 @@ function tmux_automatically_attach_session
       echo "This is on screen."
     end
   else
-    if shell_has_started_interactively; and not is_ssh_running
+    if shell_has_started_interactively && not is_ssh_running
       if not is_exists 'tmux'
         echo 'Error: tmux command not found' 2>&1
         return 1
       end
 
-      if tmux has-session >/dev/null 2>&1; and tmux list-sessions | grep -qE '.*]$'
+      if tmux has-session >/dev/null 2>&1 && tmux list-sessions | grep -qE '.*]$'
         # detached session exists
         tmux list-sessions
         echo -n "Tmux: attach? (y/N/num) "
         read
-        if "$REPLY" =~ ^[Yy]$; or "$REPLY" = ''
+        if "$REPLY" =~ '^[Yy]$' || "$REPLY" = ''
           tmux attach-session
           if $status -eq 0
             echo "(tmux -V) attached session"
             return 0
           end
-        else if "$REPLY" =~ ^[0-9]+$
+        else if "$REPLY" =~ '^[0-9]+$'
           tmux attach -t "$REPLY"
           if $status -eq 0
             echo "(tmux -V) attached session"
@@ -64,13 +64,13 @@ function tmux_automatically_attach_session
         end
       end
 
-      if is_osx; and is_exists 'reattach-to-user-namespace'
+      if is_osx && is_exists 'reattach-to-user-namespace'
         # on OS X force tmux's default command
         # to spawn a shell in the user's namespace
         set tmux_config (cat $HOME/.tmux.conf <(echo 'set-option -g default-command "reattach-to-user-namespace -l $SHELL"'))
-        tmux -f <(echo "$tmux_config") new-session; and echo "(tmux -V) created new session supported OS X"
+        tmux -f <(echo "$tmux_config") new-session && echo "(tmux -V) created new session supported OS X"
       else
-        tmux new-session; and echo "tmux created new session"
+        tmux new-session && echo "tmux created new session"
       end
     end
   end
